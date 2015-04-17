0c95d0e66f4329e7092a5bb6e039499579934807
Yotam Oron <yotamoron@gmail.com>
2015-04-17 22:33:04 +0300

    This seems to be working...

--- 30DaysCommitment/src/main/java/com/kruvi/thirtyDays/Main.java
+++ 30DaysCommitment/src/main/java/com/kruvi/thirtyDays/Main.java
@@ -4,13 +4,17 @@ import com.outbrain.ob1k.server.Server;
 import com.outbrain.ob1k.server.jetty.build.ServerBuilder;
 import com.outbrain.swinfra.metrics.api.*;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
 /**
  * Created by yotam on 4/10/15.
  */
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final ServerBuilder serverBuilder = new ServerBuilder();
 
         Server server = serverBuilder
